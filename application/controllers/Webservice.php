c31f2e95f5502ad2f0b1b4c66ac4fa3b5bcf7561
Mario Olivera <Mario.Olivera96@gmail.com>
2018-02-14 20:03:57 -0300

    no message

--- application/controllers/Webservice.php
+++ application/controllers/Webservice.php
@@ -1,5 +1,8 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+header('Content-type: application/json');
+header('Access-Control-Allow-Origin: *');
+
 class Webservice extends CI_Controller
 {
     public $funciones_generales;
@@ -11,6 +14,29 @@ class Webservice extends CI_Controller
         
     }
     
+    public function get_inicio_sesion($usuario=null,$password=null)
+    {
+        $respuesta = false;
+
+        $this->load->model("Usuario_model");
+
+        if($usuario != "" && $usuario != null && $password != "" && $password != null)
+        {
+            $this->load->library("Md5");
+            
+            $password = Md5::cifrar($password);
+
+            $usuario_response= $this->Usuario_model->get_usuario_inicio_sesion($usuario,$password);
+
+            if($usuario_response && $usuario_response["estado"]==1)
+            {
+                $respuesta = true;
+            }
+        }
+        
+        echo json_encode($respuesta);
+    }
+
     public function get_productos()
     {
         $this->load->model("Stock_productos_model");
